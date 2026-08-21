5b59b622e3d82d0cbd9c6f5de8e32025d55852e2
Benjamin Saunders <freezecubed@gmail.com>
2026-08-20 22:43:51 -0700

    First commit

---
+++ template.py
@@ -0,0 +1,25 @@
+from enum import Enum
+from dataclasses import dataclass
+from abc import ABC, abstractmethod
+from typing import List
+
+class Move(Enum):
+    ROCK     = "ROCK"
+    PAPER    = "PAPER"
+    SCISSORS = "SCISSORS"
+
+@dataclass
+class RoundResult:
+    my_move      : Move
+    opponent_move: Move
+    result       : str # String name of your class
+
+
+class RSPTemplate(ABC):
+    """
+    Inherit this template and implement the make_move function
+    """
+    
+    @abstractmethod
+    def make_move(self, history: List[RoundResult]) -> Move:
+        pass
